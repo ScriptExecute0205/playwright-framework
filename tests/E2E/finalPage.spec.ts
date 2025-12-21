3c98e79a8bfca1293b12e802d25341af9f5b6c46
Shruthi <heyyoupositiveone@gmail.com>
2025-12-21 12:07:22 +0530

    Enginnr A has updated the url

--- tests/E2E/finalPage.spec.ts
+++ tests/E2E/finalPage.spec.ts
@@ -29,7 +29,7 @@ test.describe("Final Page Validation", () => {
 
         await page.goto(BASE_URL);
         await loginPage.login(USERNAME, PASSWORD);
-        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html/Engineer B");
         await productPage.getSpecificProductDetails(productsToCart);
         await productPage.clickOnCartLink();
         await cartPage.clickCheckoutButton();
