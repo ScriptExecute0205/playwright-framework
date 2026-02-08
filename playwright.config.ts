01ac498081bd08ce2e97a4a882a675f0db0b438c
Script and Execute <heyyoupositiveone@gmail.com>
2026-02-08 17:11:42 +0530

    Add Playwright Allure workflow with merged report1

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   workers : 4,
 // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ 
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
