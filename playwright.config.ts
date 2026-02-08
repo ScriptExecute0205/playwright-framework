9d91f27a7d9688a1ff6712a85f7af304fcb24fea
Script and Execute <heyyoupositiveone@gmail.com>
2026-02-08 17:06:04 +0530

    Add Playwright Allure workflow with merged report1

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  
+  reporter: [
+    ['allure-playwright']
+  ],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
